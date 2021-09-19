ca59a123b588e5b41a8d503aac6df008ac0756f2
Avi <avner.moshkovitz@gmail.com>
2021-09-19 01:22:48 -0700

    use develop-col (with normal webview) for development

--- app/src/main/assets/file/custom.js
+++ app/src/main/assets/file/custom.js
@@ -4,3 +4,11 @@ Use it to modify the website with javascript.
 */
 
 console.log("file://custom.js loaded");
+
+// $(window).onChange3(function () {
+//     console.log('BEG Scenebar::window.onChange3');
+// });
+
+onChange4 = function () {
+    console.log('BEG onChange4');
+};
